2515aa2a4b586c774c572aa1bb79d2f188ca9533
NazarElinskii <nazar.elinskii96@mail.ru>
2018-10-28 16:44:30 +0300

    update events for day

--- src/eventDay.js
+++ src/eventDay.js
@@ -1,9 +1,10 @@
 (function addInput() {
     var day = document.getElementsByClassName("day");
     var input=document.createElement('input');
+    var taskTittle=document.getElementsByClassName('task-tittle');
     [].forEach.call(day,function(el,item){
-        el.addEventListener('click', function (e) {
-            e.preventDefault();
+        el.addEventListener('click', function (event) {
+            event.preventDefault();
             if (!this.classList.contains('task')){
                 day[item].appendChild(input);
                 input.focus();
@@ -12,14 +13,13 @@
                 this.classList.remove('task');
                 input.value="";     
         });
+        el.addEventListener('keyup',function(event){
+            event.preventDefault();
+            if(event.keyCode==13){
+                taskTittle[item].innerHTML=input.value;
+                input.value=""; 
+                input.blur();
+            }
+        })
     });
-    input.addEventListener('keyup',function(event){
-        event.preventDefault();
-        if(event.keyCode==13){
-            var taskTittle=document.getElementById('task-tittle');
-            taskTittle.innerHTML=input.value;
-            input.value=""; 
-            input.blur();
-        }
-    })
 }());
