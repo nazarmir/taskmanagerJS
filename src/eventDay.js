84fb3365c5db7bfb906702f1e3f217729a28816f
NazarElinskii <nazar.elinskii96@mail.ru>
2018-10-28 17:02:52 +0300

    add title and update daysGeneration

--- src/eventDay.js
+++ src/eventDay.js
@@ -1,7 +1,6 @@
-(function addInput() {
+(function addEventOnDay() {
     var day = document.getElementsByClassName("day");
     var input=document.createElement('input');
-    var taskTittle=document.getElementsByClassName('task-tittle');
     [].forEach.call(day,function(el,item){
         el.addEventListener('click', function (event) {
             event.preventDefault();
@@ -16,10 +15,12 @@
         el.addEventListener('keyup',function(event){
             event.preventDefault();
             if(event.keyCode==13){
-                taskTittle[item].innerHTML=input.value;
+                var taskTittle=document.createElement('span');
+                taskTittle.className='task-tittle';
+                taskTittle.innerHTML=input.value;
                 input.value=""; 
-                input.blur();
                 day[item].removeChild(input);
+                day[item].appendChild(taskTittle);
             }
         })
     });
